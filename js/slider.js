c22c21ae457f45419d54843e565bfd9a679481f8
opielapatryk <xpatrykopiela@gmail.com>
2022-08-10 09:01:31 +0200

    e

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
 const section = document.querySelector("#slider")
-const img_url = ['1.png', '2.png']
+const img_url = ['../images/1.png', '../images/2.png']
 
 let counter = 0
 
 const changeBackground = ()=>{
     counter >= 1 ? counter = 0 : counter += 1
-    section.style.backgroundImage=`url(./${img_url[counter]})`
+    section.style.backgroundImage=`url(../images/${img_url[counter]})`
 }
 setInterval(changeBackground,2000)
