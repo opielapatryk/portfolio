aafe46eda11a0efd8ab7d0eff1bf1de5bf19844c
opielapatryk <xpatrykopiela@gmail.com>
2022-08-11 11:12:29 +0200

    i

--- js/hamburger.js
+++ js/hamburger.js
@@ -21,3 +21,15 @@ buttons.addEventListener('click',(e)=>{
     menu.style.display = 'block'
     buttons.style.display = 'none'
 })
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
