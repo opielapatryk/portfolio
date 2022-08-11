965e0d552943934ec0012815a97e1b7cbf59a203
opielapatryk <xpatrykopiela@gmail.com>
2022-08-11 12:30:38 +0200

    e

--- js/hamburger.js
+++ js/hamburger.js
@@ -13,23 +13,27 @@ menu.addEventListener('mouseleave',(e)=>{
 })
 
 menu.addEventListener('click',(e)=>{
-    menu.style.display = 'none'
-    buttons.style.display = 'block'
+  if(!menu.classList.contains('opened')){
+    menu.classList.toggle('opened');
+    menu.setAttribute('aria-expanded', 
+    menu.classList.contains('opened'))
+  }
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  setTimeout(() => {
+      menu.style.display = 'none'
+      buttons.style.display = 'block'
+    }, 1000);
+} else{
+  menu.style.display = 'none'
+      buttons.style.display = 'block'
+}
+    
+    
 })
 
 buttons.addEventListener('click',(e)=>{
-    menu.style.display = 'block'
-    buttons.style.display = 'none'
+      menu.style.display = 'block'
+      buttons.style.display = 'none'
+    
 })
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "yellow";
-  } else {
-    document.body.style.backgroundColor = "pink";
-  }
-}
-
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
