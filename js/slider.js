52933ca2615d5969d28bace673fff3441d1ebe1c
opielapatryk <xpatrykopiela@gmail.com>
2022-08-11 13:05:29 +0200

    e

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,8 @@
-const section = document.querySelector("#slider")
 const img_url = ['../images/1.png', '../images/2.png']
-
-let counter = 0
-
-const changeBackground = ()=>{
-    counter >= 1 ? counter = 0 : counter += 1
-    section.style.backgroundImage=`url(../images/${img_url[counter]})`
+const img = document.querySelectorAll('.slider')
+function chngimg() {       
+    img.forEach(e=>{
+        e.classList.toggle('hidden')
+    })
 }
-setInterval(changeBackground,2000)
+setInterval(chngimg,2000)
