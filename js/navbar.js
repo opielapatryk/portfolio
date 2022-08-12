fcca70eb28b4d704615d316a3cfbf1fea50dd1de
opielapatryk <xpatrykopiela@gmail.com>
2022-08-12 22:28:04 +0200

    e

--- js/navbar.js
+++ js/navbar.js
@@ -3,11 +3,8 @@ window.onscroll = function() {
 let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector("nav").style.top = "0";
-    document.querySelector('ul').classList.remove('hoverul')
   } else {
     document.querySelector("nav").style.top = "-90px";
-    document.querySelector('ul').classList.add('hoverul')
-
   }
   prevScrollpos = currentScrollPos;
 }
